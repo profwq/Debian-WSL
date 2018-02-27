4f1160ed706e4c1c3f78b4a7ac096a7fcaaac8c2
Robert Haist <rhaist@mailbox.org>
2018-02-27 20:55:14 +0100

    Set right upstream repos for debian updates

--- create-targz.sh
+++ create-targz.sh
@@ -10,7 +10,7 @@ DIST="stretch"
 
 cd $TMPDIR
 
-sudo cdebootstrap -a $ARCH --include=sudo,locales,apt-transport-https $DIST $DIST http://deb.debian.org/debian
+sudo cdebootstrap -a $ARCH --include=sudo,locales $DIST $DIST http://deb.debian.org/debian
 
 sudo chroot $DIST apt-get clean
 
@@ -18,9 +18,11 @@ sudo chroot $DIST /bin/bash -c "update-locale LANGUAGE=en_US.UTF-8 LC_ALL=C"
 
 sudo chroot $DIST /bin/bash -c "echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen && locale-gen"
 
-sudo chroot $DIST /bin/bash -c "echo 'deb https://deb.debian.org/debian stable main' > /etc/apt/sources.list"
+sudo chroot $DIST /bin/bash -c "echo 'deb http://deb.debian.org/debian stretch main' > /etc/apt/sources.list"
 
-sudo chroot $DIST /bin/bash -c "echo 'deb https://deb.debian.org/debian-security stable/updates main' >> /etc/apt/sources.list"
+sudo chroot $DIST /bin/bash -c "echo 'deb http://deb.debian.org/debian stretch-updates main' >> /etc/apt/sources.list"
+
+sudo chroot $DIST /bin/bash -c "echo 'deb http://security.debian.org/debian-security/ stretch/updates main' >> /etc/apt/sources.list"
 
 cd $DIST
 
