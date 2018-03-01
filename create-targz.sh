115dc5da8638badfe6348bc6b6dfe0d96374771f
Robert Haist <robert.haist@dcso.de>
2018-03-01 15:25:31 +0100

    Less echo - more copy

--- create-targz.sh
+++ create-targz.sh
@@ -18,13 +18,9 @@ sudo chroot $DIST /bin/bash -c "update-locale LANGUAGE=en_US.UTF-8 LC_ALL=C"
 
 sudo chroot $DIST /bin/bash -c "echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen && locale-gen"
 
-sudo chroot $DIST /bin/bash -c "echo 'deb http://deb.debian.org/debian stretch main' > /etc/apt/sources.list"
-
-sudo chroot $DIST /bin/bash -c "echo 'deb http://deb.debian.org/debian stretch-updates main' >> /etc/apt/sources.list"
-
-sudo chroot $DIST /bin/bash -c "echo 'deb http://security.debian.org/debian-security/ stretch/updates main' >> /etc/apt/sources.list"
-
 sudo cp $BUILDIR/linux_files/profile $TMPDIR/$DIST/etc/profile
+
+sudo cp $BUILDIR/linux_files/sources.list $TMPDIR/$DIST/etc/apt/sources.list
 
 cd $DIST
 
