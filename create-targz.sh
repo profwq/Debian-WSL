9ff51567c14166d8983d8980422e8963fee4af6c
Robert Haist <rha@debian.org>
2024-11-20 12:26:40 +0100

    Prepare for new WSL tar based installations

--- create-targz.sh
+++ create-targz.sh
@@ -2,24 +2,36 @@
 
 # NOTE: you need to have the following packages installed for this to work:
 # sudo apt install qemu-user-static binfmt-support debootstrap
+#
+# Based on: https://learn.microsoft.com/en-us/windows/wsl/build-custom-distro#configuration-file-recommendations
 
 set -e
 
 BUILDIR=$(pwd)
-TMPDIR=$(mktemp -d)
-TMPDIR_ARM64=$(mktemp -d)
+
+mkdir -p $BUILDIR/rootfs
+mkdir -p $BUILDIR/debcache
+
+TMPDIR=$(mktemp -d -p $BUILDIR/rootfs)
+TMPDIR_ARM64=$(mktemp -d -p $BUILDIR/rootfs)
 
 DIST="bookworm"
 
 create_x64_rootfs() {
 	cd $TMPDIR
 
-	sudo debootstrap --arch "amd64" --exclude=debfoster --include=sudo,locales $DIST $DIST http://deb.debian.org/debian
+	sudo debootstrap --arch "amd64" --cache-dir=$BUILDIR/debcache --exclude=debfoster --include=sudo,locales $DIST $DIST http://deb.debian.org/debian
 	sudo chroot $DIST apt-get clean
-	sudo chroot $DIST /bin/bash -c "update-locale LANGUAGE=en_US.UTF-8 LC_ALL=C"
 	sudo chroot $DIST /bin/bash -c "echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen && locale-gen"
+	sudo chroot $DIST /bin/bash -c "update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8"
 	sudo cp $BUILDIR/linux_files/profile $TMPDIR/$DIST/etc/profile
 	sudo cp $BUILDIR/linux_files/sources.list $TMPDIR/$DIST/etc/apt/sources.list
+	sudo cp $BUILDIR/linux_files/wsl-distribution.conf $TMPDIR/$DIST/etc/wsl-distribution.conf
+	sudo cp $BUILDIR/linux_files/wsl.conf $TMPDIR/$DIST/etc/wsl.conf
+	sudo mkdir -p $TMPDIR/$DIST/usr/lib/wsl/
+	sudo cp $BUILDIR/linux_files/oobe.sh $TMPDIR/$DIST/usr/lib/wsl/oobe.sh
+	sudo cp $BUILDIR/pictures/debian_logo.ico $TMPDIR/$DIST/usr/lib/wsl/debian_logo.ico
+	sudo rm -f $TMPDIR/$DIST/etc/resolv.conf
 
 	cd $DIST
 	sudo tar --ignore-failed-read -czvf $TMPDIR/install.tar.gz *
@@ -32,15 +44,21 @@ create_x64_rootfs() {
 create_arm64_rootfs() {
 	cd $TMPDIR_ARM64
 
-	sudo debootstrap --arch "arm64" --foreign --exclude=debfoster --include=sudo,locales $DIST $DIST http://deb.debian.org/debian
+	sudo debootstrap --arch "arm64" --cache-dir=$BUILDIR/debcache --foreign --exclude=debfoster --include=sudo,locales $DIST $DIST http://deb.debian.org/debian
 	sudo cp $(which qemu-aarch64-static) $DIST/usr/bin
 	sudo chroot $DIST /debootstrap/debootstrap --second-stage
 	sudo chroot $DIST apt-get clean
-	sudo chroot $DIST /bin/bash -c "update-locale LANGUAGE=en_US.UTF-8 LC_ALL=C"
 	sudo chroot $DIST /bin/bash -c "echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen && locale-gen"
+	sudo chroot $DIST /bin/bash -c "update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8"
 	sudo rm -f $TMPDIR_ARM64/$DIST/usr/bin/qemu-aarch64-static
 	sudo cp $BUILDIR/linux_files/profile $TMPDIR_ARM64/$DIST/etc/profile
 	sudo cp $BUILDIR/linux_files/sources.list $TMPDIR_ARM64/$DIST/etc/apt/sources.list
+	sudo cp $BUILDIR/linux_files/wsl-distribution.conf $TMPDIR_ARM64/$DIST/etc/wsl-distribution.conf
+	sudo cp $BUILDIR/linux_files/wsl.conf $TMPDIR_ARM64/$DIST/etc/wsl.conf
+	sudo mkdir -p $TMPDIR_ARM64/$DIST/usr/lib/wsl/
+	sudo cp $BUILDIR/linux_files/oobe.sh $TMPDIR_ARM64/$DIST/usr/lib/wsl/oobe.sh
+	sudo cp $BUILDIR/pictures/debian_logo.ico $TMPDIR_ARM64/$DIST/usr/lib/wsl/debian_logo.ico
+	sudo rm -f $TMPDIR_ARM64/$DIST/etc/resolv.conf
 
 	cd $DIST
 	sudo tar --ignore-failed-read -czvf $TMPDIR_ARM64/install_arm64.tar.gz *
